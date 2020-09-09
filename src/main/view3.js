b2a9c82f618fe7ca8e660a59fad4347e8d1aa612
Dylan Thomson <thomsodyla1@ecs.vuw.ac.nz>
2020-09-09 13:34:53 +1200

    added a template navigation structure

---
+++ src/main/view3.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class View3 extends Component {
+  render() {
+    return (
+        <div>
+          <h2>View 3</h2>
+        </div>
+    );
+  }
+}
